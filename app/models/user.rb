798dbbde1b7da209574a4f796d510ba20b302fe5
Anton Antonov <anton.synd.antonov@gmail.com>
2015-05-25 00:50:51 +0300

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_create :create_activation_digest
+  before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -10,10 +12,6 @@ class User < ActiveRecord::Base
                     length: { maximum: 250 }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_nil: true
-
-  before_save do
-    self.email = email.downcase
-  end
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -35,8 +33,28 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def activate!
+    update(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,7 +68,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'authenticated? should return false for a user with nil digest' do
-    assert_not @user.authenticated?(nil)
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, nil)
+    assert_not @user.authenticated?(:remember, '')
   end
 end
